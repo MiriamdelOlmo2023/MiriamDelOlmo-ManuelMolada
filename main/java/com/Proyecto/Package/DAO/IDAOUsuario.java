dc2762e8eed780234c56ef9a7264eb0548a6d014
MiriamdelOlmo2023 <148791098+MiriamdelOlmo2023@users.noreply.github.com>
2024-02-26 10:42:07 +0100

    Delete main directory

--- main/java/com/Proyecto/Package/DAO/IDAOUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.Proyecto.Package.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.Proyecto.Package.Entity.UsuarioEntity;
-
-public interface IDAOUsuario  extends CrudRepository<UsuarioEntity, String>{
-
-}
--- main/resources/static/burger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () { 
-    const menuHamburguesa = document.getElementById('menuHamburguesa');
-    const nav = document.querySelector('nav');  
-    menuHamburguesa.addEventListener('click', function () {
-        nav.classList.toggle('mostrar');
-        menuHamburguesa.classList.toggle('transicion')    
-    });  
-});
