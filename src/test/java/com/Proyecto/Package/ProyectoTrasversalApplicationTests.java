b77a5621564d3be2477224d769c85144e76355eb
MiriamdelOlmo2023 <148791098+MiriamdelOlmo2023@users.noreply.github.com>
2024-02-26 10:27:33 +0100

    Add files via upload

---
+++ src/test/java/com/Proyecto/Package/ProyectoTrasversalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.Package;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTrasversalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
